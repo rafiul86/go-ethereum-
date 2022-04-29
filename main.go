b2309df5d30092b90cc60fb31df20d1dcdd8abae
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-29 15:15:25 +0600

    For loop

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello, World!");
+func main() {
+	cards := []int{2, 4, 6}
+	cards = append(cards, 8)
+	for index,  card := range cards {
+		fmt.Println(index, card)
+	}
 }
