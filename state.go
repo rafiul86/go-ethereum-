dc3f54913ddc0bddb2fa303f31691654c400e53f
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-04-29 17:46:07 +0600

    Type

--- state.go
+++ state.go
@@ -1,5 +1,8 @@
 package main
 
+import "fmt"
+
+
 type deck []string
 
 func newDeck() deck {
@@ -7,10 +10,16 @@ func newDeck() deck {
 
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
-	for _, suits := range cardSuits {
-		for _, values := range cardValues {
-			cards = append(cards, suits+" of "+values+" ,")
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
+}
+
+func(d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
 }
