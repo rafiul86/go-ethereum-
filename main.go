76320742a1e560f3e4b255b9c6c7bfe98c7bf1e6
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-01 23:33:00 +0600

    String concatenation

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	greetings := "Hello"
-	greetings = (greetings + " World")
-	fmt.Printf("%v %T", greetings, greetings)
+	var (
+	num1 uint64 = 10e13
+	num2 uint64 = 23e14
+	)
+	fmt.Println(num1+num2)
 }
