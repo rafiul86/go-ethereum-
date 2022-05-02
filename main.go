c23618e0f38a55d4e0ba945c11e4d285e9b35346
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-02 12:46:38 +0600

    Slice and array dot operator

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-num := [...] int {1,2,3,4,5}
-num2 := num
+num := [] int {1,2,3,4,5}
+num2 := [] int {6,7,8,9,10}
+num3 := append(num, num2...)
 num[3] = 22
 fmt.Println(num)
 fmt.Println(num2)
+fmt.Println(num3)
 }
