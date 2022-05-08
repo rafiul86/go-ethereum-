e10d4eba4cc699027ad614a3a88c16e93fd66ad9
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-05-08 17:28:14 +0600

    return result

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
-import "fmt"
-type people struct {
-	count string
-	time int
-}
-type person struct {
-	people
-	name string
-	age int
-}
+import (
+	"fmt"
+)
+
 func main() {
-Loop:
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			if i*j >= 15 { break Loop }
-			fmt.Println(i, "*", j, "=", i*j)
-		}
-		}
+	nyNum := printNum(12, 14)
+	fmt.Println(nyNum)
+}
+
+func printNum (num1 int, num2 int) (result int) {
+	result = num1 + num2
+	return
 }
