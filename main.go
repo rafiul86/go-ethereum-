765114748cac7a5c75745b4f9ca8193716617204
RAFIUL HASAN CHOWDHURY <68476971+rafiul86@users.noreply.github.com>
2022-06-08 14:55:06 +0600

    Gessing game bug fixed

--- main.go
+++ main.go
@@ -1,27 +1,33 @@
 package main
 
 import (
-	"app/doctor"
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
-// main function
+const prompt = "and press Enter when ready."
+// main function of Guess number game
 func main() {
-	sayHello := doctor.Intro()
-	fmt.Println(sayHello)
+	// declare essential variables
+
+	var firstNumber = 2
+    var secondNumber = 5
+	var subtraction = 7
+	var answer int
 	reader := bufio.NewReader(os.Stdin)
-	
-	for {
-		fmt.Print("-> ")
-		userInput, _ := reader.ReadString('\n')
-		userInput = strings.Replace(userInput, "\r\n", "", -1)
-		if userInput == "quit" {
-			break
-		} else {
-			fmt.Println(doctor.Response(userInput))
-		}
-	}
+	fmt.Println("Welcome to the Guess Number Game!")
+	fmt.Println("---------------------------------------------------")
+	fmt.Println("Think of a number between 1 and 10", prompt)
+	reader.ReadString('\n')
+	fmt.Println("Multiply your nymber by", firstNumber, prompt)
+	reader.ReadString('\n')
+	fmt.Println("Multiply the result by", secondNumber, prompt)
+	reader.ReadString('\n')
+	fmt.Println("Divide the result the number by you originally thought of",  prompt)
+	reader.ReadString('\n')
+	fmt.Println("Now Subtract", subtraction, prompt)
+	reader.ReadString('\n')
+	answer = firstNumber * secondNumber - subtraction
+	fmt.Println("Your answer is", answer)
 }
