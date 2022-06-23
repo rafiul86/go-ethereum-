1bae6cc9b4f737c5de33de2ab53c8591727358e3
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-06-23 12:54:54 +0600

    update operating system arguments

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	var a float64 = 7.1
-
-	x, y := true, 3.7
-
-	c, x := 5.5, false
-
-	_, _, _, _ = c, x, y, a
+	fmt.Println(os.Args[2])
 }
