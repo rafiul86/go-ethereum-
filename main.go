a5e67ce14a6a16af1f9e6b7e9d5a870cb0bed60e
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-06-25 15:43:06 +0600

    variadic parameters

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("%f\n", add(1.08, 2.06, 3.06, 4.07, 5.90))
+	num := []float64{1.08, 2.06, 3.06, 4.07, 5.90}
+	fmt.Printf("%f\n", add(num...))
 }
 
 func add(values ...float64) float64 {
