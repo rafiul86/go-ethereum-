3f55309ec11775699571a7dcb08434d57ff6f81d
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-07-04 23:34:20 +0600

    keyed slice initialization

--- main.go
+++ main.go
@@ -3,15 +3,7 @@ package main
 import "fmt"
  
 func main() {
-	age := -9
-	switch{
-	case age < 0 || age > 100:
-		fmt.Println("Invalid Age")
-	case age < 18 :
-		fmt.Println("You are minor!")
-	case age == 18 :
-		fmt.Println("Congratulations! You've just become major!")
-	default:
-		fmt.Println("You are major!")
-	}
+	nums := [...]string{1: "Hi", 3: "Hello", 5: "World", "OMG"}
+	
+	fmt.Println(nums)
 }
