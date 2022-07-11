9fe03ab0296bb494408c1d844fb02f43eb250857
rafiul hasan chowdhury <rhasan346@gmail.com>
2022-07-11 23:16:46 +0600

    error handling for file creationj

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
  
 func main() {
-	nums := [...]string{1: "Hi", 3: "Hello", 5: "World", "OMG"}
-	
-	fmt.Println(nums)
+	var newFile *os.File 
+	fmt.Println(newFile)
+
+	newFile, err := os.Create("test.txt")
+	os.WriteFile("test.txt", []byte("Hello World"), 0644)
+	if err != nil {
+		fmt.Println(err, newFile)
+	}
 }
